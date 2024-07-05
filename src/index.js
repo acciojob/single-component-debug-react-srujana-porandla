7d695b95e7d3ab64d352317ba71148ebae9228ce
Srujana <p.srujana1356@gmail.com>
2024-07-05 14:18:40 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-  <App />
-);
-
+ReactDOM.render(
+    <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
